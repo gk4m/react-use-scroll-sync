c9fa82c2a8f7d44ec269b5aed542200987941c02
Grzegorz Kaminski <kaminski_grzegorz@wp.pl>
2020-02-16 11:17:19 +0100

    feat: add useScrollSync hook

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,168 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import "react-app-polyfill/ie11"
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { useSrollSync } from "../."
 
 const App = () => {
-  return (
-    <div>
-      <Thing />
-    </div>
-  );
-};
+  const ref1 = React.useRef<HTMLDivElement>(null)
+  const ref2 = React.useRef<HTMLDivElement>(null)
+  const ref3 = React.useRef<HTMLDivElement>(null)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  useSrollSync([ref1, ref2, ref3])
+
+  return (
+    <>
+      <div
+        ref={ref1}
+        style={{
+          overflow: "auto",
+          maxWidth: "300px",
+          maxHeight: "200px"
+        }}
+      >
+        <p style={{ width: "500px" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          dictum sagittis augue, quis scelerisque mauris euismod vitae.
+          Vestibulum elit nisi, porta ut elit tincidunt, eleifend fermentum
+          mauris. Proin sapien massa, elementum quis imperdiet sit amet, feugiat
+          vitae mi. Praesent bibendum mi ac ultricies feugiat. Interdum et
+          malesuada fames ac ante ipsum primis in faucibus. Donec vel dolor
+          eleifend, imperdiet lorem vel, consectetur mi. Praesent ultricies
+          libero non nulla elementum porta. Morbi viverra varius libero. Donec
+          dolor magna, sodales at commodo ac, aliquet eget magna. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Suspendisse dictum
+          sagittis augue, quis scelerisque mauris euismod vitae. Vestibulum elit
+          nisi, porta ut elit tincidunt, eleifend fermentum mauris. Proin sapien
+          massa, elementum quis imperdiet sit amet, feugiat vitae mi. Praesent
+          bibendum mi ac ultricies feugiat. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Donec vel dolor eleifend, imperdiet lorem
+          vel, consectetur mi. Praesent ultricies libero non nulla elementum
+          porta. Morbi viverra varius libero. Donec dolor magna, sodales at
+          commodo ac, aliquet eget magna.
+        </p>
+      </div>
+      <br />
+      <div
+        ref={ref2}
+        style={{
+          overflow: "auto",
+          maxWidth: "300px",
+          maxHeight: "200px"
+        }}
+      >
+        <p style={{ width: "500px" }}>
+          x Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          dictum sagittis augue, quis scelerisque mauris euismod vitae.
+          Vestibulum elit nisi, porta ut elit tincidunt, eleifend fermentum
+          mauris. Proin sapien massa, elementum quis imperdiet sit amet, feugiat
+          vitae mi. Praesent bibendum mi ac ultricies feugiat. Interdum et
+          malesuada fames ac ante ipsum primis in faucibus. Donec vel dolor
+          eleifend, imperdiet lorem vel, consectetur mi. Praesent ultricies
+          libero non nulla elementum porta. Morbi viverra varius libero. Donec
+          dolor magna, sodales at commodo ac, aliquet eget magna. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Suspendisse dictum
+          sagittis augue, quis scelerisque mauris euismod vitae. Vestibulum elit
+          nisi, porta ut elit tincidunt, eleifend fermentum mauris. Proin sapien
+          massa, elementum quis imperdiet sit amet, feugiat vitae mi. Praesent
+          bibendum mi ac ultricies feugiat. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Donec vel dolor eleifend, imperdiet lorem
+          vel, consectetur mi. Praesent ultricies libero non nulla elementum
+          porta. Morbi viverra varius libero. Donec dolor magna, sodales at
+          commodo ac, aliquet eget magna.
+        </p>
+      </div>
+
+      <div
+        ref={ref3}
+        style={{
+          overflow: "auto",
+          maxWidth: "300px",
+          maxHeight: "300px"
+        }}
+      >
+        <p style={{ width: "1100px" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          dictum sagittis augue, quis scelerisque mauris euismod vitae.
+          Vestibulum elit nisi, porta ut elit tincidunt, eleifend fermentum
+          mauris. Proin sapien massa, elementum quis imperdiet sit amet, feugiat
+          vitae mi. Praesent bibendum mi ac ultricies feugiat. Interdum et
+          malesuada fames ac ante ipsum primis in faucibus. Donec vel dolor
+          eleifend, imperdiet lorem vel, consectetur mi. Praesent ultricies
+          libero non nulla elementum porta. Morbi viverra varius libero. Donec
+          dolor magna, sodales at commodo ac, aliquet eget magna. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Suspendisse dictum
+          sagittis augue, quis scelerisque mauris euismod vitae. Vestibulum elit
+          nisi, porta ut elit tincidunt, eleifend fermentum mauris. Proin sapien
+          massa, elementum quis imperdiet sit amet, feugiat vitae mi. Praesent
+          bibendum mi ac ultricies feugiat. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Donec vel dolor eleifend, imperdiet lorem
+          vel, consectetur mi. Praesent ultricies libero non nulla elementum
+          porta. Morbi viverra varius libero. Donec dolor magna, sodales at
+          commodo ac, aliquet eget magna. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Suspendisse dictum sagittis augue, quis
+          scelerisque mauris euismod vitae. Vestibulum elit nisi, porta ut elit
+          tincidunt, eleifend fermentum mauris. Proin sapien massa, elementum
+          quis imperdiet sit amet, feugiat vitae mi. Praesent bibendum mi ac
+          ultricies feugiat. Interdum et malesuada fames ac ante ipsum primis in
+          faucibus. Donec vel dolor eleifend, imperdiet lorem vel, consectetur
+          mi. Praesent ultricies libero non nulla elementum porta. Morbi viverra
+          varius libero. Donec dolor magna, sodales at commodo ac, aliquet eget
+          magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Suspendisse dictum sagittis augue, quis scelerisque mauris euismod
+          vitae. Vestibulum elit nisi, porta ut elit tincidunt, eleifend
+          fermentum mauris. Proin sapien massa, elementum quis imperdiet sit
+          amet, feugiat vitae mi. Praesent bibendum mi ac ultricies feugiat.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
+          vel dolor eleifend, imperdiet lorem vel, consectetur mi. Praesent
+          ultricies libero non nulla elementum porta. Morbi viverra varius
+          libero. Donec dolor magna, sodales at commodo ac, aliquet eget magna.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          dictum sagittis augue, quis scelerisque mauris euismod vitae.
+          Vestibulum elit nisi, porta ut elit tincidunt, eleifend fermentum
+          mauris. Proin sapien massa, elementum quis imperdiet sit amet, feugiat
+          vitae mi. Praesent bibendum mi ac ultricies feugiat. Interdum et
+          malesuada fames ac ante ipsum primis in faucibus. Donec vel dolor
+          eleifend, imperdiet lorem vel, consectetur mi. Praesent ultricies
+          libero non nulla elementum porta. Morbi viverra varius libero. Donec
+          dolor magna, sodales at commodo ac, aliquet eget magna. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Suspendisse dictum
+          sagittis augue, quis scelerisque mauris euismod vitae. Vestibulum elit
+          nisi, porta ut elit tincidunt, eleifend fermentum mauris. Proin sapien
+          massa, elementum quis imperdiet sit amet, feugiat vitae mi. Praesent
+          bibendum mi ac ultricies feugiat. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Donec vel dolor eleifend, imperdiet lorem
+          vel, consectetur mi. Praesent ultricies libero non nulla elementum
+          porta. Morbi viverra varius libero. Donec dolor magna, sodales at
+          commodo ac, aliquet eget magna. malesuada fames ac ante ipsum primis
+          in faucibus. Donec vel dolor eleifend, imperdiet lorem vel,
+          consectetur mi. Praesent ultricies libero non nulla elementum porta.
+          Morbi viverra varius libero. Donec dolor magna, sodales at commodo ac,
+          aliquet eget magna. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Suspendisse dictum sagittis augue, quis scelerisque mauris
+          euismod vitae. Vestibulum elit nisi, porta ut elit tincidunt, eleifend
+          fermentum mauris. Proin sapien massa, elementum quis imperdiet sit
+          amet, feugiat vitae mi. Praesent bibendum mi ac ultricies feugiat.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
+          vel dolor eleifend, imperdiet lorem vel, consectetur mi. Praesent
+          ultricies libero non nulla elementum porta. Morbi viverra varius
+          libero. Donec dolor magna, sodales at commodo ac, aliquet eget magna.
+          malesuada fames ac ante ipsum primis in faucibus. Donec vel dolor
+          eleifend, imperdiet lorem vel, consectetur mi. Praesent ultricies
+          libero non nulla elementum porta. Morbi viverra varius libero. Donec
+          dolor magna, sodales at commodo ac, aliquet eget magna. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Suspendisse dictum
+          sagittis augue, quis scelerisque mauris euismod vitae. Vestibulum elit
+          nisi, porta ut elit tincidunt, eleifend fermentum mauris. Proin sapien
+          massa, elementum quis imperdiet sit amet, feugiat vitae mi. Praesent
+          bibendum mi ac ultricies feugiat. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Donec vel dolor eleifend, imperdiet lorem
+          vel, consectetur mi. Praesent ultricies libero non nulla elementum
+          porta. Morbi viverra varius libero. Donec dolor magna, sodales at
+          commodo ac, aliquet eget magna.
+        </p>
+      </div>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ src/scroll-sync.ts
@@ -0,0 +1,85 @@
+import { RefObject, useEffect } from "react"
+
+export type Options = {
+  horizontal?: boolean
+  vertical?: boolean
+  proportional?: boolean
+}
+
+const defaultOptions: Options = {
+  horizontal: true,
+  vertical: true,
+  proportional: true
+}
+
+type ScrollSync = <T extends HTMLElement>(
+  refs: RefObject<T>[],
+  options?: Options
+) => void
+
+const updateScrollsPosition = <T extends HTMLElement>(
+  target: HTMLElement,
+  refs: RefObject<T>[],
+  options: Options
+) => {
+  const scrollLeftOffset =
+    target.scrollLeft / (target.scrollWidth - target.offsetWidth)
+
+  const scrollTopOffset =
+    target.scrollTop / (target.scrollHeight - target.offsetHeight)
+
+  refs.forEach(({ current }) => {
+    if (!current) return
+
+    if (options.vertical) {
+      const position = options.proportional
+        ? scrollTopOffset * (current.scrollHeight - current.offsetHeight)
+        : target.scrollTop
+
+      current.scrollTop = Math.round(position)
+    }
+
+    if (options.horizontal) {
+      const position = options.proportional
+        ? scrollLeftOffset * (current.scrollWidth - current.offsetWidth)
+        : target.scrollLeft
+
+      current.scrollLeft = Math.round(position)
+    }
+  })
+}
+
+export const useSrollSync: ScrollSync = (refs, options) => {
+  if (refs.length < 2) {
+    throw Error("You need to pass at least two refs")
+  }
+
+  const scrollSyncOptions = {
+    ...defaultOptions,
+    ...options
+  }
+
+  const handleScroll = ({ target }: Event) => {
+    if (!target) throw Error("Event target shouldn't be null")
+
+    const refsWithoutTarget = refs.filter(({ current }) => current !== target)
+
+    updateScrollsPosition(
+      target as HTMLElement,
+      refsWithoutTarget,
+      scrollSyncOptions
+    )
+  }
+
+  useEffect(() => {
+    refs.forEach(({ current }) =>
+      current?.addEventListener("scroll", handleScroll)
+    )
+
+    return () => {
+      refs.forEach(({ current }) =>
+        current?.removeEventListener("scroll", handleScroll)
+      )
+    }
+  }, [refs])
+}
